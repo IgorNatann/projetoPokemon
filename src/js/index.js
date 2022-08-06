03c88cda429b30e0e81a9566f53077808ac4d615
Igor Natan <igornatan4@gmail.com>
2022-08-05 22:08:37 -0300

    commit versão final projeto

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,33 @@
-/*
- 
- OBJETIVO 1 -  quando clicarmos na seta de avançar, temos que mostrar o próximo cartão da lista.
-    - passo 1 -  dar umm jeito de pegar o elemento HTML da seta avançar.
-    - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar.
-    - passo 3 - fazer aparecer o próximo cartão da lista.
-    - passo 4 - buscar o cartão que está selecionado e esconder.
-
- OBJETIVO 2 -  quando clicarmos na seta de voltar, temos que mostrar o cartão anterior da lista.
-    - passo 1 -  dar umm jeito de pegar o elemento HTML da seta voltar.
-    - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar.
-    - passo 3 - fazer aparecer o anterior cartão da lista.
-    - passo 4 - buscar o cartão que está selecionado e esconder.    
-
- */
-
-// - passo 1 -  dar umm jeito de pegar o elemento HTML da seta avançar.
 const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
 
-//  - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar.
+function escondedrCartaoSelecionado() {
+   const cartaoSelecionado = document.querySelector(".selecionado");
+   cartaoSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao(indiceCartao) {
+   cartoes[indiceCartao].classList.add("selecionado");
+}
 btnAvancar.addEventListener("click", function () {
-  //- passo 3 - fazer aparecer o próximo cartão da lista.
-  const cartoes = document.querySelectorAll(".cartao");
-  cartoes[1].classList.add("selecionado");
+
+   if (cartaoAtual === cartoes.length - 1) return;
+
+   escondedrCartaoSelecionado();
+
+   cartaoAtual++;
+   mostrarCartao(cartaoAtual);
+
+});
+
+btnVoltar.addEventListener("click", function () {
+
+   if (cartaoAtual === 0) return;
+
+   escondedrCartaoSelecionado();
+
+   cartaoAtual--;
+   mostrarCartao(cartaoAtual);
 });
